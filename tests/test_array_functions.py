e35b9a6174f71a81df857b519376b3ae3fc50239
geirksa <geirksa@ifi.uio.no>
2023-03-15 12:58:42 +0100

    added test

---
+++ tests/test_array_functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from logarray import log_array
+from logarray.testing import assert_logarray_allclose
+import pytest
+
+test_arrays = [np.array([1,2,3]), np.array([-1,-2,-3]), np.array([1,2,-3])]
+@pytest.mark.parametrize("regular", test_arrays)
+def test_sum(regular):
+    mylogarray = log_array(regular)
+
+    #assert logarray.sum() == np.sum(logarray)
+    assert(np.sum(mylogarray), regular.sum())
+    logarray_sum = np.log(np.sum(mylogarray))
+    regular_sum = np.log(np.sum(regular))
+    assert_logarray_allclose(logarray_sum, regular_sum)
+
