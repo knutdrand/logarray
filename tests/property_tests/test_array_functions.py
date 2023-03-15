dbb994d77d393ed1dc046af10b8061d291ff4898
geirksa <geirksa@ifi.uio.no>
2023-03-15 15:20:01 +0100

    updated hypothesis test

--- tests/property_tests/test_array_functions.py
+++ tests/property_tests/test_array_functions.py
@@ -15,16 +15,16 @@ def test_is_valid_array():
     assert not is_valid_array(np.array([False, True], dtype=bool))
 @composite
 def valid_arrays(draw):
-    return draw(stnp.arrays(shape=(4, ), dtype=stnp.integer_dtypes()|stnp.floating_dtypes()))
+    return draw(stnp.arrays(shape=(4, ), dtype=stnp.floating_dtypes()))
 
-@given(valid_arrays().filter(lambda a: np.all(a > 0)))
+@given(valid_arrays())
 @example(np.array([1,1,1,1]))
-@pytest.mark.skip("FAILING")
+@pytest.mark.skip("Failing, due to lacking support for negative numbers")
 def test_sum(regular):
     mylogarray = log_array(regular)
 
     logarray_sum = np.sum(mylogarray)
     regular_sum = np.sum(regular)
-    assert_logarray_allclose(logarray_sum, regular_sum, rtol=1e-3)
+    assert_logarray_allclose(logarray_sum, regular_sum, rtol=1e-2 if regular.dtype=='float16' else 1e-5)
 
 print(valid_arrays().example())
