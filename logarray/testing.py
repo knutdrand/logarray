6a999440344cc03d481373779f19934d3ad0a0c5
geirksa <geirksa@ifi.uio.no>
2023-03-15 15:10:45 +0100

    added hypothesis test

--- logarray/testing.py
+++ logarray/testing.py
@@ -3,10 +3,10 @@ from . import LogArray
 import numpy as np
 
 
-def assert_logarray_allclose(*arrays):
+def assert_logarray_allclose(*arrays, rtol=1e-7, atol=0):
     arrays = [array.to_array() if isinstance(array, LogArray)
               else array for array in arrays]
-    assert_allclose(*arrays)
+    assert_allclose(*arrays, rtol=rtol, atol=atol)
 
 
 def assert_logarray_allclose_log_space(array1, array2):
