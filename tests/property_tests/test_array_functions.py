43d4e914f933d5f343b9067af16e2978bcd3b6b8
geirksa <geirksa@ifi.uio.no>
2023-03-17 11:47:51 +0100

    Added product test, not passing

--- tests/property_tests/test_array_functions.py
+++ tests/property_tests/test_array_functions.py
@@ -1,12 +1,13 @@
 import pytest
 from hypothesis import given, example
 import hypothesis.extra.numpy as stnp
+import hypothesis.strategies as st
 from hypothesis.strategies import composite
 from logarray import log_array
 from logarray.testing import assert_logarray_allclose
 import numpy as np
 
-from tests.test_array_functions import _test_multiply
+from tests.test_array_functions import _test_array_function, array_functions, array_functions_future
 
 VALID_LOGARRAY_DTYPES = [np.integer, np.floating]
 def is_valid_array(array):
@@ -20,7 +21,6 @@ def valid_arrays(draw):
     return draw(stnp.arrays(shape=(4, ), dtype=stnp.floating_dtypes()))
 
 @given(valid_arrays())
-@example(np.array([1,1,1,1]))
 @pytest.mark.skip("Failing, due to lacking support for negative numbers")
 def test_sum(regular):
     mylogarray = log_array(regular)
@@ -29,7 +29,13 @@ def test_sum(regular):
     regular_sum = np.sum(regular)
     assert_logarray_allclose(logarray_sum, regular_sum, rtol=1e-2 if regular.dtype=='float16' else 1e-5)
 
-@given(valid_arrays())
+@given(regular=valid_arrays(), array_func=st.sampled_from(array_functions))
+@example(array_func=np.sum, regular=np.array([1,1,1,1]))
+def test_array_function(regular, array_func):
+    _test_array_function(regular, array_func)
+
+@given(regular=valid_arrays(), array_func=st.sampled_from(array_functions_future))
+@example(array_func=np.sum, regular=np.array([1,1,1,1]))
 @pytest.mark.skip('fail')
-def test_multiply(regular):
-    _test_multiply(regular)
+def test_array_function_future(regular, array_func):
+    _test_array_function(regular, array_func)
--- tests/test_array_functions.py
+++ tests/test_array_functions.py
@@ -4,6 +4,8 @@ from logarray import log_array
 from logarray.testing import assert_logarray_allclose
 import pytest
 
+array_functions = [np.sum]
+array_functions_future = [np.product]
 test_arrays = [np.array([1,2,3]), np.array([-1,-2,-3]), np.array([1,2,-3])]
 @pytest.mark.skip('fail')
 @pytest.mark.parametrize("regular", test_arrays)
@@ -14,15 +16,22 @@ def test_sum(regular):
     regular_sum = np.log(np.sum(regular))
     assert_logarray_allclose(logarray_sum, regular_sum)
 
-def _test_multiply(regular):
+def _test_array_function(regular, array_func):
     mylogarray = log_array(regular)
 
-    logarray_product = np.product(mylogarray)
-    regular_product = np.product(regular)
+    logarray_product = array_func(mylogarray)
+    regular_product = array_func(regular)
     assert_logarray_allclose(logarray_product, regular_product, rtol=1e-2 if regular.dtype=='float16' else 1e-5)
 
 
+
+@pytest.mark.parametrize("array_func", array_functions)
 @pytest.mark.parametrize("regular", test_arrays)
-@pytest.mark.skip('fail')
-def test_multiply(regular):
-    _test_multiply(regular)
+def test_array_function(regular, array_func):
+    _test_array_function(regular, array_func)
+
+@pytest.mark.parametrize("array_func", array_functions_future)
+@pytest.mark.parametrize("regular", test_arrays)
+@pytest.mark.xfail('fail')
+def test_array_function_future(regular, array_func):
+    _test_array_function(regular, array_func)
